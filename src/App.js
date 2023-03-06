81f63f09f3bca53cfac027c36dde5fedb7c5255e
Mark Masulis <mark.masulis@sjsu.edu>
2023-03-06 01:57:23 -0800

    Added starter files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import { Route, Router } from "react-router-dom";
+import React from 'react'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route
+} from "react-router-dom"
 
-function App() {
+
+export default function App() {
+
+  //Every time you want to add a new page path (baseurl.com/my_path) do it here
+  //If you want to make a subpage/subcomponent, (baseurl.com/my_path/subpath), add another router in whatever component /my_path routes to
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<p>Landing Page Component Goes Here!</p>}/>
+        <Route path="/some_other_path" element={<p>Some other page's component goes here</p>}/>
+      </Route>
+    )
+  );
+
+  //If some components will be present throughout the site, like a navigation bar, then this RouterProvider can be passed as a prop or surrounded by the component.
   return (
-    <Router>
-      <Route exact path="/">
-        {/*landing page component*/}
-      </Route>
-      <Route path="/some_other_route">
-        {/*Some other component goes here*/}
-      </Route>
-    </Router>
+    <RouterProvider router={router} />
   );
 }
-
-export default App;
---
+++ src/Restaurant/RestaurantHomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route
+} from "react-router-dom"
+
+export default function RestaurantHomePage(props){
+    
+}
