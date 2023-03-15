25e2843f1c67fe5f58c3ffeb1bd99be7d25d10d4
pfarrow75 <pfarrow75@gmail.com>
2023-03-14 18:27:54 -0700

    Basic restraunt page, need to setup history for restraunt, as well as waiting for API

--- src/Restaurant/Restaurant.js
+++ src/Restaurant/Restaurant.js
@@ -1,13 +1,42 @@
-import React from 'react'
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route
-} from "react-router-dom"
-
-export default function Restaurant(props){
-    return(
-        <p>This is the main component for restaurant users</p>
-    )
+import React, { useState } from "react";
+import { Routes, Route, useParams } from "react-router-dom";
+import TabBar from "./../Components/TabBar";
+import RestaurantHomePage
+ from "./RestaurantHomePage";
+export default function Restaurant(props) {
+  const { userType } = useParams();
+  const [userTab, setUserTab] = useState(userType || "create");
+  return (
+    <div>
+      <TabBar
+        tabs={[
+          {
+            label: "Create",
+            onSelect: () => {
+              setUserTab("create");
+            },
+          },
+          { label: "History",
+          onSelect: () => {
+            setUserTab("order history");
+          }, },
+        ]}
+        defaultLabel={userType}
+      />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={(() => {
+            switch (userTab) {
+              case "create":
+                return <RestaurantHomePage />;
+            }
+            
+          })()}
+        />
+      </Routes>
+    </div>
+  );
 }
+
