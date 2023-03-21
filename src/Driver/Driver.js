4f84098934480f34ab76579efe87cfd28073660b
pfarrow75 <pfarrow75@gmail.com>
2023-03-21 14:43:29 -0700

    adding driver page to have toolbar

--- src/Driver/Driver.js
+++ src/Driver/Driver.js
@@ -1,13 +1,60 @@
-import React from 'react'
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route
-} from "react-router-dom"
+import React, { useState } from "react";
+import { 
+  Routes, 
+  Route, 
+  useNavigate, 
+  useParams, 
+  useSearchParams 
+} from "react-router-dom";
+import TabBar from "./../Components/TabBar";
+import RestaurantHomePage
+ from "./DriverHomePage";
+import RestaurantOrders from './DriverHistory'
+import RestaurantProfile from './DriverProfile'
+import DriverHomePage from "./DriverHomePage";
+import DriverProfile from "./DriverProfile";
+import DriverHistory from "./DriverHistory";
+export default function Driver(props) {
+  const { userType } = useParams();
+  const [userTab, setUserTab] = useState(userType || "create");
+  const [searchParams, setSearchParams] = useSearchParams()
+  const token = searchParams.get("token")
+  const navigate = useNavigate()
 
-export default function Driver(props){
-    return(
-        <p>This is the main component for driver users</p>
-    )
+  return (
+    <div>
+      <TabBar
+        tabs={[
+          {
+            label: "Home",
+            onSelect: () => {
+              setUserTab("home");
+              navigate(`/driver/home?token=${token}`)
+            },
+          },
+          { 
+            label: "History",
+            onSelect: () => {
+              setUserTab("order history");
+              navigate(`/driver/history?token=${token}`)
+            }, 
+          },
+          {
+            label: "Profile",
+            onSelect: () => {
+              setUserTab("profile");
+              navigate(`/driver/profile?token=${token}`)
+            }
+          }
+        ]}
+        defaultLabel={userType}
+      />
+      <Routes>
+        <Route path="/home" element={<DriverHomePage token={token}/>}/>
+        <Route path="/profile" element={<DriverProfile token={token}/>}/>
+        <Route path="/history" element={<DriverHistory token={token}/>}/>
+      </Routes>
+    </div>
+  );
 }
+
--- src/Restaurant/RestaurantOrders.js
+++ src/Restaurant/RestaurantOrders.js
@@ -2,14 +2,18 @@ import * as React from "react";
 import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
 
 const rows = [
-  { id: 1, col1: "Hello", col2: "World" },
+  { id: 1, col1: "Josh", col2: "20 min", col3: "000001", col4:"123 S Main Street", col5:"3/16/2023", col6:"Peter" },
   { id: 2, col1: "DataGridPro", col2: "is Awesome" },
   { id: 3, col1: "MUI", col2: "is Amazing" },
 ];
 
 const columns = [
-  { field: "col1", headerName: "Column 1", width: 150 },
-  { field: "col2", headerName: "Column 2", width: 150 },
+  { field: "col1", headerName: "Driver", width: 150 },
+  { field: "col2", headerName: "ETA", width: 150 },
+  { field: "col3", headerName: "Order Number", width: 150 },
+  { field: "col4", headerName: "Customer Address", width: 300 },
+  { field: "col5", headerName: "Date", width: 150 },
+  { field: "col6", headerName: "Customer Name", width: 150 },
 ];
 
 export default function App() {
@@ -18,7 +22,7 @@ export default function App() {
       <div
         style={{
           height: 300,
-          width: "80%",
+          width: "60%",
           display: "inline-block",
         }}
       >
