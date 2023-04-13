866737417b37d8a859409aaba3ea5744720f856d
pfarrow75 <pfarrow75@gmail.com>
2023-04-12 19:38:36 -0700

    got get requests up and working, need to manipulate some values to make it readable, but it finally works.

--- src/Driver/DriverHistory.js
+++ src/Driver/DriverHistory.js
@@ -1,43 +1,83 @@
-import React from 'react'
 
+import React, {
+  useState,
+  useEffect
+} from 'react'
 import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route
-} from "react-router-dom"
-const rows = [
-  { id: 1, col1: "Josh", col2: "20 min", col3: "000001", col4:"123 S Main Street", col5:"3/16/2023", col6:"Peter" },
-  { id: 2, col1: "DataGridPro", col2: "is Awesome" },
-  { id: 3, col1: "MUI", col2: "is Amazing" },
-];
 
-const columns = [
-  { field: "col1", headerName: "Driver", width: 150 },
-  { field: "col2", headerName: "ETA", width: 150 },
-  { field: "col3", headerName: "Order Number", width: 150 },
-  { field: "col4", headerName: "Customer Address", width: 300 },
-  { field: "col5", headerName: "Date", width: 150 },
-  { field: "col6", headerName: "Customer Name", width: 150 },
-];
 
 export default function DriverHistory(props) {
-
-
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+  const [data, setData] = useState()
+  const [rows, setRows] =useState([]);
   
+  const getOrdersData = () => {
+    const token = props.token
+    if (!token){
+        setError(true)
+        return
+    }
+    fetch(process.env.REACT_APP_API + '/driver/orders?pageSize=10&page=1', {
+        method: "GET",
+        headers: {
+            "content-type": "application/json",
+            access_token: token,
+        }
+    }).then(
+        (response) => response.json()
+    ).then(
+        (data) => {
+            setLoading(false)
+            
+            setData(data.data)
+            setRows(data.data.data)
+            switch(data.code){
+                case 200: //good things are happening :)
+                    break;
+                default: //bad things are happening :(
+                    setError(true)
+                    break
+            }
+        }
+    )
+  }
+  const columns = rows.length > 0 ? Object.keys(rows[0]).map((key) => {
+    if (key === "Id") {
+      return {
+        field: key,
+        headerName: key.charAt(0).toUpperCase() + key.slice(1),
+        width: 200,
+        renderCell: (params) => (
+          <button onClick={() => handleClick(params.value)}>Click me</button>
+        )
+      };
+    } else {
+      return {
+        field: key,
+        headerName: key.charAt(0).toUpperCase() + key.slice(1),
+        width: 200,
+      };
+    }
+  }) : [];
+
+  const handleClick = (value) => {
+    console.log("Hello world");
+  };
+
+  useEffect(getOrdersData, [])
   return (
     <div style={{ textAlign: 'center' }}>
       <div
         style={{
-          height: 300,
-          width: "60%",
+          height: 600,
+          width: "90%",
           display: "inline-block",
         }}
       >
         <DataGrid
           rows={rows}
-          columns={columns}
+          columns= {columns}
         />
       </div>
     </div>
