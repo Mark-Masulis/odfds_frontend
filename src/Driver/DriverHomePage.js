f3e22da37f1a1bfc760bc3118268b05236824094
Mark Masulis <markmasulis@gmail.com>
2023-04-17 17:16:23 -0700

    Finished location provider component

--- src/Driver/DriverHomePage.js
+++ src/Driver/DriverHomePage.js
@@ -7,20 +7,41 @@ import {
 } from "react-router-dom"
 import Notification from './Notification'
 import OrderTracker from './OrderTracker'
+import { Button } from '../Components/StaticComponents'
 
 export default function DriverHomePage(props){
-  return(<OrderTracker
-    frequencySeconds={10}
-    token={props.token}
-    active={true}
-    onOrderReceived={() => {
-      alert("RECEIVED")
-    }}
-    onOrerRejected={() => {
-      alert("REJECTED")
-    }}
-    onNoLocation={() => {
-      alert("NO LOCATION")
-    }}
-  />)
+
+  const [active, setActive] = useState(false)
+
+  return(
+  <div>
+    <Button
+      onClick={() => {setActive(!active)}}
+    >
+      {active ? "Deactivate" : "Activate"}
+    </Button>
+    <OrderTracker
+      frequencySeconds={5}
+      token={props.token}
+      active={active}
+      onOrderReceived={(data) => {
+        alert("There is a new order available!")
+      }}
+      onOrderRejected={(data) => {
+        alert("The order timed out or was rejected.")
+      }}
+      onOrderAccept={(data) => {
+        alert("The order was accepted")
+      }}
+      onOrderPickup={(data) => {
+        alert("The order was picked up")
+      }}
+      onNoLocation={(data) => {
+        alert("Unable to access location.")
+      }}
+      onError={(error) => {
+        alert(JSON.stringify(error))
+      }}
+    />
+  </div>)
 }
--- src/Driver/OrderTracker.js
+++ src/Driver/OrderTracker.js
@@ -6,74 +6,68 @@ import useWebSocket from 'react-use-websocket'
 
 //props.token = the JWT associated with this driver user
 //props.active = whether or not the socket is active and sending/receiving data from server
+//props.frequencySeconds = how often in seconds location should be reported
+//THE FOLLOWING FUNCTIONS ARE OPTIONAL
 //props.onOrderReceived = the function called when a new order is received
 //props.onOrderRejected = the function called when a received order times out or is manually rejected
+//props.onOrderAccept = the function called when an available order is accepted
+//props.onOrderPickup = the function called when an accepted order is picked up
 //props.onNoLocation = the function called when the browser doesn't allow the user to access location
-//props.frequencySeconds = how often in seconds location should be reported
+//props.onError = the function called when an error is returned from the server
 export default function OrderTracker(props){
-
-    const [active, setActive] = useState(props.active)
     const [timerId, setTimerId] = useState()
-    const [latitude, setLatitude] = useState()
-    const [longitude, setLongitude] = useState()
 
-    const wsAddress = process.env.REACT_APP_WS_URL + '/driver/locationWebsocket?access_token=' + props.token
+    const wsAddress = process.env.REACT_APP_WS_URL + '/driver/location?access_token=' + props.token
 
     const {sendJsonMessage, readyState} = useWebSocket(wsAddress, {
         onMessage: (event) => {
             var data = event.data
-            alert(data)
             switch(data.code){
                 case 200:
                     //success
                     break
                 case 201:
                     //A new order has been received
-                    props.onOrderReceived()
+                    props.onOrderReceived(data.data)
                     break
                 case 202:
                     //An order still needs to be accepted
                     break
                 case 203:
                     //pending order was rejected
-                    props.onOrderRejected()
+                    props.onOrderRejected(data.data)
                     break
                 case 204:
                     //pending order was accepted
+                    props.onOrderAccept(data.data)
                     break
                 case 205:
                     //order was picked up
+                    props.onOrderPickup(data.data)
                     break
                 case 206:
                     //order was delivered
+                    props.onDeliver(data.data)
                     break
                 default:
+                    if(data.code > 400 || JSON.parse(data).code > 400){
+                        props.onError(data)
+                    }
                     //something is wrong
                     break
             }
-        },
-        onOpen: () => {
-            
-        },
-        onClose: () => {
-            alert("CLOSING")
         }
     })
 
     const deactivate = () => {
-        setActive(false)
         if(timerId){
             clearInterval(timerId)
         }
     }
 
     const activate = () => {
-        setActive(true)
         setTimerId(setInterval(() => {
-            if(active){
-                getLocation()
-                sendJsonMessage({latitude: latitude, longitude: longitude})
-            }
+            getLocation()
         }, props.frequencySeconds * 1000))
     }
 
@@ -85,19 +79,19 @@ export default function OrderTracker(props){
             getLocation()
         }
     }, [props.active])
-    
+
     const getLocation = () => {
         if(navigator.geolocation){
             const coords = navigator.geolocation.getCurrentPosition((position) => {
                 //success
-                setLatitude(position.coords.latitude)
-                setLongitude(position.coords.longitude)
+                sendJsonMessage({latitude: position.coords.latitude, longitude: position.coords.longitude})
             }, (error) => {
-                //failure - skip this attempt and hope the next one works
-
+                //failure
+                deactivate()
+                props.onNoLocation()
             })
         }else{
-            //handle no navigation enabled by browser
+            //handle no navigation allowed by browser
             deactivate()
             props.onNoLocation()
         }
