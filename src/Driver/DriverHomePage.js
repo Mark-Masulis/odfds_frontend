51ee525326de2a6cd15c2734f530532da101f431
Mark Masulis <markmasulis@gmail.com>
2023-04-17 17:41:48 -0700

    Placed order tracker and notification component in driver component

--- src/Driver/DriverHomePage.js
+++ src/Driver/DriverHomePage.js
@@ -1,47 +1,6 @@
 import React, { useState } from 'react'
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route
-} from "react-router-dom"
-import Notification from './Notification'
-import OrderTracker from './OrderTracker'
-import { Button } from '../Components/StaticComponents'
 
+//props.onActivationStateChange = the function called when activation is toggled. passes the new state as a bool parameter.
 export default function DriverHomePage(props){
 
-  const [active, setActive] = useState(false)
-
-  return(
-  <div>
-    <Button
-      onClick={() => {setActive(!active)}}
-    >
-      {active ? "Deactivate" : "Activate"}
-    </Button>
-    <OrderTracker
-      frequencySeconds={5}
-      token={props.token}
-      active={active}
-      onOrderReceived={(data) => {
-        alert("There is a new order available!")
-      }}
-      onOrderRejected={(data) => {
-        alert("The order timed out or was rejected.")
-      }}
-      onOrderAccept={(data) => {
-        alert("The order was accepted")
-      }}
-      onOrderPickup={(data) => {
-        alert("The order was picked up")
-      }}
-      onNoLocation={(data) => {
-        alert("Unable to access location.")
-      }}
-      onError={(error) => {
-        alert(JSON.stringify(error))
-      }}
-    />
-  </div>)
 }
