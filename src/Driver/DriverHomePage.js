bfd91cc5aefadb453cfd52f66f480a6bc0e2362d
LX-2020 <62913062+LX-2020@users.noreply.github.com>
2023-04-19 10:21:01 -0700

    use OrderTracker in DriverHomePage

--- src/Driver/DriverHomePage.js
+++ src/Driver/DriverHomePage.js
@@ -1,19 +1,126 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route
-} from "react-router-dom"
-import TabBar from "./../Components/TabBar"
+  Routes,
+  Route,
+  useNavigate,
+  useParams,
+  useSearchParams,
+  useLocation
+} from 'react-router-dom'
+
+import TabBar from './../Components/TabBar'
 import DriverHistory from './DriverHistory'
 import DriverProfile from './DriverProfile'
-import OrderTracker from "./OrderTracker"
+import OrderTracker from './OrderTracker'
 
+export default function DriverHomePage(props) {
+  const { userType } = useParams()
+  const location = useLocation()
+  const pathParts = location.pathname.split('/')
+  const [userTab, setUserTab] = useState(userType || 'create')
+  const [searchParams, setSearchParams] = useSearchParams()
+  const token = props.token
+  const navigate = useNavigate()
+  const [status, setStatus] = useState('')
+  const [isActivated, setIsActivated] = useState(false)
 
-import TabBar from "./../Components/TabBar"
-import DriverHistory from './DriverHistory'
-import DriverProfile from './DriverProfile'
-export default function DriverHomePage(props){
-    
+  const handleToggle = () => {
+    setIsActivated(!isActivated)
+  }
+
+  return (
+    <div>
+      <div>
+        <Routes>
+          <Route
+            path="/home"
+            element={
+              <>
+                <TabBar
+                  tabs={[
+                    {
+                      label: 'Home',
+                      onSelect: () => {
+                        setUserTab('home')
+                        navigate(`/driver/home?token=${token}`)
+                      }
+                    },
+                    {
+                      label: 'History',
+                      onSelect: () => {
+                        setUserTab('history')
+                        navigate(`/driver/history?token=${token}`)
+                      }
+                    },
+                    {
+                      label: 'Account',
+                      onSelect: () => {
+                        setUserTab('account')
+                        navigate(`/driver/profile?token=${token}`)
+                      }
+                    }
+                  ]}
+                  defaultLabel={pathParts[pathParts.length - 1]}
+                />
+              </>
+            }
+          />
+          <Route path="/account" element={<DriverProfile token={token} />} />
+          <Route path="/history" element={<DriverHistory token={token} />} />
+        </Routes>
+      </div>
+
+      <div
+        className="status"
+        style={{
+          marginTop: '80px',
+          marginBottom: '280px',
+          marginLeft: '200px',
+          marginRight: '180px',
+          display: 'flex',
+          alignItems: 'space-around'
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Current Status:"
+          value={status}
+          readOnly
+          style={{ marginRight: '10px' }}
+        />
+
+        <button 
+        onClick={handleToggle}
+        style={{           
+            backgroundColor: '#0C695D',
+            fontFamily: 'Inter',
+            color: 'white',
+            fontSize: '20px',
+            border: 0,
+            borderRadius: '5px',
+            padding: '10px 50px',
+            '&:hover': {
+            backgroundColor: '#37AFA9',
+            cursor: 'pointer',
+            }
+        }}
+        >
+  {isActivated ? 'Deactivate' : 'Activate'}
+</button>
+      </div>
+
+      <OrderTracker
+        token={token}
+        active={isActivated}
+        frequencySeconds={5}
+        onOrderReceived={(data) => console.log('Order received:', data)}
+        onOrderRejected={(data) => console.log('Order rejected:', data)}
+        onOrderAccepted={(data) => console.log('Order accepted:', data)}
+        onOrderPickup={(data) => console.log('Order pickup:', data)}
+        onNoLocation={() => console.log('Unable to access location.')}
+        onError={(error) => console.log('Error:', error)}
+        onUnverified={(data) => console.log('Unverified account:', data)}
+      />
+    </div>
+  )
 }
