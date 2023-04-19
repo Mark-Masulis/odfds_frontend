e7dab2ce14d07715bcd353f9877f121503f09f37
Mark Masulis <markmasulis@gmail.com>
2023-04-19 15:33:44 -0700

    Fixed order tracking and driver profile

--- src/Driver/DriverHomePage.js
+++ src/Driver/DriverHomePage.js
@@ -1,75 +1,18 @@
 import React, { useState, useEffect } from 'react'
-import {
-  Routes,
-  Route,
-  useNavigate,
-  useParams,
-  useSearchParams,
-  useLocation
-} from 'react-router-dom'
+import { Button } from '../Components/StaticComponents'
 
-import TabBar from './../Components/TabBar'
-import DriverHistory from './DriverHistory'
-import DriverProfile from './DriverProfile'
-import OrderTracker from './OrderTracker'
-
+//props.onActivationStateChange = funciton called when activate state is toggled. Gives the NEW state as a param
 export default function DriverHomePage(props) {
-  const { userType } = useParams()
-  const location = useLocation()
-  const pathParts = location.pathname.split('/')
-  const [userTab, setUserTab] = useState(userType || 'create')
-  const [searchParams, setSearchParams] = useSearchParams()
   const token = props.token
-  const navigate = useNavigate()
-  const [status, setStatus] = useState('')
   const [isActivated, setIsActivated] = useState(false)
 
   const handleToggle = () => {
     setIsActivated(!isActivated)
+    props.onActivationStateChange(!isActivated)
   }
 
   return (
     <div>
-      <div>
-        <Routes>
-          <Route
-            path="/home"
-            element={
-              <>
-                <TabBar
-                  tabs={[
-                    {
-                      label: 'Home',
-                      onSelect: () => {
-                        setUserTab('home')
-                        navigate(`/driver/home?token=${token}`)
-                      }
-                    },
-                    {
-                      label: 'History',
-                      onSelect: () => {
-                        setUserTab('history')
-                        navigate(`/driver/history?token=${token}`)
-                      }
-                    },
-                    {
-                      label: 'Account',
-                      onSelect: () => {
-                        setUserTab('account')
-                        navigate(`/driver/profile?token=${token}`)
-                      }
-                    }
-                  ]}
-                  defaultLabel={pathParts[pathParts.length - 1]}
-                />
-              </>
-            }
-          />
-          <Route path="/account" element={<DriverProfile token={token} />} />
-          <Route path="/history" element={<DriverHistory token={token} />} />
-        </Routes>
-      </div>
-
       <div
         className="status"
         style={{
@@ -89,38 +32,12 @@ export default function DriverHomePage(props) {
           style={{ marginRight: '10px', border: 0, fontSize: '20px', }}
         />
 
-        <button 
-        onClick={handleToggle}
-        style={{           
-            backgroundColor: '#0C695D',
-            fontFamily: 'Inter',
-            color: 'white',
-            fontSize: '20px',
-            border: 0,
-            borderRadius: '5px',
-            padding: '10px 50px',
-            '&:hover': {
-            backgroundColor: '#37AFA9',
-            cursor: 'pointer',
-            }
-        }}
+        <Button 
+            onClick={handleToggle}
         >
-  {isActivated ? 'Deactivate' : 'Activate'}
-</button>
+            {isActivated ? 'Deactivate' : 'Activate'}
+        </Button>
       </div>
-
-      <OrderTracker
-        token={token}
-        active={isActivated}
-        frequencySeconds={5}
-        onOrderReceived={(data) => console.log('Order received:', data)}
-        onOrderRejected={(data) => console.log('Order rejected:', data)}
-        onOrderAccepted={(data) => console.log('Order accepted:', data)}
-        onOrderPickup={(data) => console.log('Order pickup:', data)}
-        onNoLocation={() => console.log('Unable to access location.')}
-        onError={(error) => console.log('Error:', error)}
-        onUnverified={(data) => console.log('Unverified account:', data)}
-      />
     </div>
   )
 }
